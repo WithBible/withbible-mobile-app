2701a63f4b0c31a28758032d23fdb07321262dc7
Brave Kim <ygflove@bible.ac.kr>
2022-05-17 19:56:46 +0900

    :white_check_mark: Finish leaderBoard screen

--- lib/screen/leader_boards_screen.dart
+++ lib/screen/leader_boards_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:withbible_app/common/theme_helper.dart';
 import 'package:withbible_app/controller/quiz_history.dart';
 import 'package:withbible_app/model/leader_board.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LeaderBoardsScreen extends StatefulWidget {
   const LeaderBoardsScreen({Key? key}) : super(key: key);
@@ -22,9 +23,14 @@ class _LeaderBoardsScreenState extends State<LeaderBoardsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('리더보드'),
+      ),
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(color: ThemeHelper.shadowColor),
+          padding: const EdgeInsets.all(16),
           child: FutureBuilder(
             future: leaderBoardFuture,
             builder: (
@@ -44,18 +50,96 @@ class _LeaderBoardsScreenState extends State<LeaderBoardsScreen> {
                   scrollDirection: Axis.vertical,
                   itemCount: snapshot.data!.length,
                   itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        snapshot.data![index].photoPath == ""
-                            ? const Text("Don't have")
-                            : const Text("Have Image"),
-                        Image.network(snapshot.data![index].photoPath)
-                      ],
-                    );
+                    return index == 0
+                        ? Container(
+                            padding: const EdgeInsets.all(10),
+                            decoration: const BoxDecoration(
+                                color: Color(0xff8d5ac4),
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(4),
+                                    bottomRight: Radius.circular(4))),
+                            child: buildTopLeaderBoardItem(snapshot.data))
+                        : buildLeaderBoardItem(index, snapshot.data);
                   });
             },
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildTopLeaderBoardItem(List<LeaderBoard>? data) {
+    const int index = 0;
+
+    return Align(
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Center(
+                child: buildLeaderBoardSizeBadge(data![index].photoPath),
+              ),
+              const Center(
+                  child: Padding(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  '1',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ))
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                data[index].username,
+                style: const TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              const Spacer(),
+              Text(
+                '✨ ${data[index].score} ✨',
+                style: const TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildLeaderBoardItem(int index, List<LeaderBoard>? data) {
+    return Row(
+      children: [
+        Text(
+          '${index + 1}',
+          style: const TextStyle(color: Colors.black, fontSize: 16),
+        ),
+        data![index].username == ""
+            ? const ClipRRect() // TODO: Need no image
+            : buildLeaderBoardSizeBadge(data[index].photoPath),
+        Text(
+          data[index].username,
+          style: const TextStyle(color: Colors.black, fontSize: 14),
+        ),
+        const Spacer(),
+        Text(
+          data[index].score,
+          style: const TextStyle(color: Colors.black, fontSize: 14),
+        ),
+      ],
+    );
+  }
+
+  Widget buildLeaderBoardSizeBadge(String photoPath) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(100),
+      child: SvgPicture.network(
+        photoPath,
+        fit: BoxFit.cover,
+        width: 70,
+        height: 70,
       ),
     );
   }
